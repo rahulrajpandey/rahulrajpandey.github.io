14b67fccbbebb7826457f5e0add626e41dc7e698
Rahul Raj <rahulrajpandey0448@gmail.com>
2017-04-11 16:18:29 +0530

    Ajax Call in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,12 @@
 'use strict';
 
 $(function(){
-	alert("hello");
+	var root = 'https://jsonplaceholder.typicode.com';
+
+	$.ajax({
+	  url: root + '/posts/1',
+	  method: 'GET'
+	}).then(function(data) {
+	  console.log(data);
+	});
 })();
