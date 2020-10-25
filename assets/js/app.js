2df4d80ed5ff148858b813288c3af7302058ccdc
Rahul Raj <Rahul.Raj@amdocs.com>
2020-10-25 23:14:48 +0530

    Initial Change for Github-Page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-$(function(){
-	console.log("Web Profile for Rahul Raj");
-	console.log("LinkedId: https://www.linkedin.com/in/rahulrajpandey/");
+$(function () {
+  console.log("Web Profile for Rahul Raj");
+  console.log("LinkedId: https://www.linkedin.com/in/rahulrajpandey/");
 });
