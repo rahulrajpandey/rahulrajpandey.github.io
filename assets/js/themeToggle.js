95614d72d14520acc0bc57a27cdafeeaa78f4815
Rahul Raj <rahulrajpandey0448@gmail.com>
2025-05-30 21:08:29 +0530

    show a cross icon when the menu is opened in mobile view

--- assets/js/themeToggle.js
+++ assets/js/themeToggle.js
@@ -23,3 +23,22 @@ themeToggle.addEventListener("click", () => {
   const current = body.getAttribute("data-theme") === "dark" ? "light" : "dark";
   setTheme(current);
 });
+
+
+// navbar toggle icon change
+document.addEventListener("DOMContentLoaded", function () {
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const navbarIcon = document.getElementById("navbar-icon");
+    const navbarCollapse = document.getElementById("navbarNav");
+
+    // Listen to Bootstrap's collapse events
+    navbarCollapse.addEventListener("show.bs.collapse", function () {
+      navbarIcon.classList.remove("bi-list");
+      navbarIcon.classList.add("bi-x");
+    });
+
+    navbarCollapse.addEventListener("hide.bs.collapse", function () {
+      navbarIcon.classList.remove("bi-x");
+      navbarIcon.classList.add("bi-list");
+    });
+  });
